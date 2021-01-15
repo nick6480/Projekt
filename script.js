6d5347f9859da84688ca9a24e480be676ae4e9a1
Nickolaj <nickolaj99@outlook.com>
2021-01-15 16:54:40 +0100

    added btn

--- script.js
+++ script.js
@@ -4,8 +4,16 @@
 function randomColor() {
   let color = Math.floor(Math.random()*16777215).toString(16);
   document.body.style.backgroundColor = "#" + color;
-  console.log(color)
+  console.log(color);
+}
+
+function createBtn() {
+let btn = document.createElement("BUTTON");
+btn.innerHTML = "Change Color";
+btn.addEventListener("click", randomColor);
+document.body.appendChild(btn);
 }
 
 
-window.addEventListener("load", randomColor);
+randomColor();
+createBtn();
