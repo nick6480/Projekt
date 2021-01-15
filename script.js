7b7493e6958826c585a04d9235bfe65a09a8ccdb
Nickolaj <nickolaj99@outlook.com>
2021-01-15 17:03:46 +0100

    added code

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-
-
-
 function randomColor() {
   let color = Math.floor(Math.random()*16777215).toString(16);
   document.body.style.backgroundColor = "#" + color;
   console.log(color);
 }
-
-
 
 randomColor();
