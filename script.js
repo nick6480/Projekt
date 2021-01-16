eadc3e2e65b737b3a4208d6ade27e7e351af7fd8
Nickolaj <nickolaj99@outlook.com>
2021-01-16 13:52:52 +0100

    test pull

--- script.js
+++ script.js
@@ -2,21 +2,15 @@ function randomColor() {
   let color = Math.floor(Math.random()*16777215).toString(16);
   document.body.style.backgroundColor = "#" + color;
   console.log(color);
-<<<<<<< HEAD
-}
-
-randomColor();
-=======
 }
 
 function createBtn() {
-let btn = document.createElement("BUTTON");
-btn.innerHTML = "Change Color";
-btn.addEventListener("click", randomColor);
-document.body.appendChild(btn);
+  let btn = document.createElement("BUTTON");
+  btn.innerHTML = "Change Color";
+  btn.addEventListener("click", randomColor);
+  document.body.appendChild(btn);
 }
 
 
 randomColor();
 createBtn();
->>>>>>> develop
